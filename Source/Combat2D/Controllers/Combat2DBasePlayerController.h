021d2187b2a18cfc69fd8f87e9ddadefa2fe5189
Dion van der Wateren <39389180+Nidolgh@users.noreply.github.com>
2021-10-30 00:53:32 +0200

    Added base character and player controller classes
    Removed Private and Public folders in Combat2D

---
+++ Source/Combat2D/Controllers/Combat2DBasePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Combat2DBasePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBAT2D_API ACombat2DBasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
